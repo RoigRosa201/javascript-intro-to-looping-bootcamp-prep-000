5e36a39eab227fad22ad31c8430f507670901f77
Roigrosa <roigrosa88@gmail.com>
2018-04-15 23:57:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,3 +8,12 @@ for ( let i = 0; i < 25; i++) {
 }
 return array;
 }
+
+function whileLoop(n) {
+  let countdown = n;
+
+  while (countdown > 0) {
+  console.log(--countdown)
+}
+  return "done";
+}
